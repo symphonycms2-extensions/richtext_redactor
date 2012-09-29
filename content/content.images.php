4e2712b7d90a20ef539087805e11dca8780c4d9a
Remie Bolte <r.bolte@gmail.com>
2012-09-29 17:47:55 +0200

    Fixing issue with empty image list

--- content/content.images.php
+++ content/content.images.php
@@ -7,12 +7,14 @@ $files = General::listStructure($root, null, false, 'asc', $root);
 
 $result = array();
 
-foreach($files['filelist'] as $file) {
-	$result[] = array(
-		'thumb' => URL . '/redactor/' . $file,
-		'image' => URL . '/redactor/' . $file,
-		'title' => $file
-	);
+if(isset($files) && isset($files['filelist'])) {
+	foreach($files['filelist'] as $file) {
+		$result[] = array(
+			'thumb' => URL . '/redactor/' . $file,
+			'image' => URL . '/redactor/' . $file,
+			'title' => $file
+		);
+	}
 }
 
 echo stripslashes(json_encode($result));

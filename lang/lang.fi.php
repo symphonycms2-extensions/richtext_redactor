861a2240ea2493c77e2ee5d233c2753b12e4195c
Leo Nikkilä <leo.nikkila@gmail.com>
2013-06-23 01:11:28 +0300

    Added two missing strings

--- lang/lang.fi.php
+++ lang/lang.fi.php
@@ -1,6 +1,6 @@
 <?php
 
-  $about = array(
+	$about = array(
 		'name' => 'Finnish',
 		'author' => array(
 			'name' => 'Leo Nikkilä',
@@ -15,76 +15,82 @@
 	 */
 	$dictionary = array(
 
-		'Set focus to Redactor editor on page load' => 
+		'Set focus to Redactor editor on page load' =>
 		'Kohdenna editoriin sivun ladattua',
 
-		'Display the Toolbar in the Redactor editor' => 
+		'Display the Toolbar in the Redactor editor' =>
 		'Näytä työkalupalkki editorissa',
 
-		'Enable the HTML source button on the toolbar' => 
+		'Enable the HTML source button on the toolbar' =>
 		'Näytä HTML-lähdekoodipainike työkalupalkissa',
 
-		'Enable air-mode' => 
+		'Enable air-mode' =>
 		'Käytä air-tilaa',
 
-		'Enable visual structure mode' => 
+		'Enable visual structure mode' =>
 		'Havainnollista tekstin rakennetta',
 
-		'Enable height autoresizing' => 
+		'Enable height autoresizing' =>
 		'Muuta editorin kokoa kirjoitettaessa',
 
-		'Enable image properties dialog' => 
+		'Enable image properties dialog' =>
 		'Salli kuvien asetusten muokkaaminen',
 
-		'Enable formatting to paragraphs' => 
+		'Enable formatting to paragraphs' =>
 		'Käytä kappalejakoa',
 
-		'Enable text cleanup on paste' => 
+		'Enable text cleanup on paste' =>
 		'Siisti teksti liitettäessä',
 
-		'Automatically replace URLs with hyperlinks' => 
+		'Automatically replace URLs with hyperlinks' =>
 		'Korvaa URL:t linkeillä automaattisesti',
 
-		'Automatically replace divs to paragraphs' => 
+		'Automatically replace divs to paragraphs' =>
 		'Korvaa div-tagit kappaleilla automaattisesti',
 
-		'Enable file upload from the Redactor editor' => 
+		'Enable file upload from the Redactor editor' =>
 		'Salli tiedostojen lataus editorista',
 
-		'Enable image upload from the Redactor editor' => 
+		'Enable image upload from the Redactor editor' =>
 		'Salli kuvien lataus editorista',
 
-		'Enable keydown / keyup shortcuts functionality' => 
+		'Enable keydown / keyup shortcuts functionality' =>
 		'Käytä ylä- ja alanuolten pikanäppäintoimintoja',
 
-		'Enable the editor for mobile devices' => 
+		'Enable the editor for mobile devices' =>
 		'Näytä editori mobiililaitteille',
 
-		'Enable overlay for modal Redactor windows' => 
+		'Enable overlay for modal Redactor windows' =>
 		'Näytä tausta valintaikkunoiden takana',
 
-		'The toolbar will remain at the top of the browser window (fixed) at all times' => 
+		'The toolbar will remain at the top of the browser window (fixed) at all times' =>
 		'Kiinnitä työkalupalkki selainikkunan yläreunaan',
 
-		'This option makes a fixed toolbar to the width of the editor (in fixed mode)' => 
+		'This option makes a fixed toolbar to the width of the editor (in fixed mode)' =>
 		'Tee työkalupalkista editorin levyinen, kun se on kiinnitetty',
 
-		'Text direction is Left-To-Right (ltr). Uncheck for Right-To-Left (rtl)' => 
+		'Text direction is Left-To-Right (ltr). Uncheck for Right-To-Left (rtl)' =>
 		'Tekstin suunta on vasemmalta oikealle (LTR). Poista raksi kirjoittaaksesi oikealta vasemmalle (RTL).',
 
-		'Toolbar Buttons' => 
+		'Toolbar Buttons' =>
 		'Työkalupalkin painikkeet',
 
-		'Air Buttons (in air mode)' => 
+		'Air Buttons (in air mode)' =>
 		'Air-painikkeet (air-tilassa)',
 
-		'Allowed Tags' => 
+		'Allowed Tags' =>
 		'Sallitut tagit',
 
-		'File upload location' => 
+		'File upload location' =>
 		'Ladattujen tiedostojen tallennuskansio',
 
-		'Image upload location' => 
+		'Image upload location' =>
 		'Ladattujen kuvien tallennuskansio',
+
+		'The directory where uploaded files are stored (default: WORKSPACE/redactor/files)' =>
+		'Kansio johon ladatut tiedostot tallennetaan (oletuksena WORKSPACE/redactor/files)',
+
+		'The directory where uploaded images are stored (default: WORKSPACE/redactor/images)' =>
+		'Kansio johon ladatut kuvat tallennetaan (oletuksena WORKSPACE/redactor/images)'
 
 	);

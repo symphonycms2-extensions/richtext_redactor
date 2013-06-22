6bcc07f6940e14d13abe50f3cb44a82fe75db8e1
Leo Nikkilä <leo.nikkila@gmail.com>
2013-06-23 01:04:16 +0300

    Added Finnish translation.

---
+++ lang/lang.fi.php
@@ -0,0 +1,90 @@
+<?php
+
+  $about = array(
+		'name' => 'Finnish',
+		'author' => array(
+			'name' => 'Leo Nikkilä',
+			'email' => 'leo.nikkila@gmail.com',
+			'website' => ''
+		),
+		'release-date' => '2013-06-23'
+	);
+
+	/**
+	 * Rich Text (Redactor) Text Formatter
+	 */
+	$dictionary = array(
+
+		'Set focus to Redactor editor on page load' => 
+		'Kohdenna editoriin sivun ladattua',
+
+		'Display the Toolbar in the Redactor editor' => 
+		'Näytä työkalupalkki editorissa',
+
+		'Enable the HTML source button on the toolbar' => 
+		'Näytä HTML-lähdekoodipainike työkalupalkissa',
+
+		'Enable air-mode' => 
+		'Käytä air-tilaa',
+
+		'Enable visual structure mode' => 
+		'Havainnollista tekstin rakennetta',
+
+		'Enable height autoresizing' => 
+		'Muuta editorin kokoa kirjoitettaessa',
+
+		'Enable image properties dialog' => 
+		'Salli kuvien asetusten muokkaaminen',
+
+		'Enable formatting to paragraphs' => 
+		'Käytä kappalejakoa',
+
+		'Enable text cleanup on paste' => 
+		'Siisti teksti liitettäessä',
+
+		'Automatically replace URLs with hyperlinks' => 
+		'Korvaa URL:t linkeillä automaattisesti',
+
+		'Automatically replace divs to paragraphs' => 
+		'Korvaa div-tagit kappaleilla automaattisesti',
+
+		'Enable file upload from the Redactor editor' => 
+		'Salli tiedostojen lataus editorista',
+
+		'Enable image upload from the Redactor editor' => 
+		'Salli kuvien lataus editorista',
+
+		'Enable keydown / keyup shortcuts functionality' => 
+		'Käytä ylä- ja alanuolten pikanäppäintoimintoja',
+
+		'Enable the editor for mobile devices' => 
+		'Näytä editori mobiililaitteille',
+
+		'Enable overlay for modal Redactor windows' => 
+		'Näytä tausta valintaikkunoiden takana',
+
+		'The toolbar will remain at the top of the browser window (fixed) at all times' => 
+		'Kiinnitä työkalupalkki selainikkunan yläreunaan',
+
+		'This option makes a fixed toolbar to the width of the editor (in fixed mode)' => 
+		'Tee työkalupalkista editorin levyinen, kun se on kiinnitetty',
+
+		'Text direction is Left-To-Right (ltr). Uncheck for Right-To-Left (rtl)' => 
+		'Tekstin suunta on vasemmalta oikealle (LTR). Poista raksi kirjoittaaksesi oikealta vasemmalle (RTL).',
+
+		'Toolbar Buttons' => 
+		'Työkalupalkin painikkeet',
+
+		'Air Buttons (in air mode)' => 
+		'Air-painikkeet (air-tilassa)',
+
+		'Allowed Tags' => 
+		'Sallitut tagit',
+
+		'File upload location' => 
+		'Ladattujen tiedostojen tallennuskansio',
+
+		'Image upload location' => 
+		'Ladattujen kuvien tallennuskansio',
+
+	);
